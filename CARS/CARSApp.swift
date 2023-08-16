0b5d44e9381a656812a9da713ec072f3f55bf093
Yogesh <yogesh.mnd@gmail.com>
2023-08-16 13:25:29 +0530

    Initial Commit

---
+++ CARS/CARSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CARSApp.swift
+//  CARS
+//
+//  Created by Yogesh on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CARSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
